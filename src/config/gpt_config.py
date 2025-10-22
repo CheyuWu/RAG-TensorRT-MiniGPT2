8d7a03429e0a45b851e56d3407794aaeb3296fdf
Cheyu Wu <cheyu1220@gmail.com>
2025-10-23 01:53:13 +0800

    fix: duplicate config
    Signed-off-by: Cheyu Wu <cheyu1220@gmail.com>

--- src/config/gpt_config.py
+++ src/config/gpt_config.py
@@ -1,7 +1,6 @@
 
 
 DATA_PATH = "src/data/sample_docs.txt"
-RETRIEVER_MODEL = "all-MiniLM-L6-v2"
 # Can be changed to "gpt2-medium", "gpt2-large", "gpt2-xl"
 GPT2_MODEL = "gpt2-medium"
 RETRIEVER_MODEL = "all-MiniLM-L6-v2"
